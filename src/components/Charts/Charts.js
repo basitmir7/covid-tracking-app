14051f9031a12e2125e5b83c30eb5e713a6ee3ba
Basit Mir <80674953+basitmir7@users.noreply.github.com>
2022-06-09 15:40:34 +0530

    initial app changes

---
+++ src/components/Charts/Charts.js
@@ -0,0 +1,9 @@
+const Charts = () => {
+  return (
+    <div>
+      <h1>Charts</h1>
+    </div>
+  );
+};
+
+export default Charts;
---
+++ src/components/CountryPicker/CountryPicker.js
@@ -0,0 +1,9 @@
+const CountryPicker = ({ data }) => {
+  return (
+    <div>
+      <h1>CountryPicker</h1>
+    </div>
+  );
+};
+
+export default CountryPicker;
